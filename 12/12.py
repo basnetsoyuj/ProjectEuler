aa3b36108fa6a2446694308664d161ddcee3321e
SoyujBasnet <bsoyuj@gmail.com>
2018-12-23 18:34:50 +0545

    ...12

--- 12/12.py
+++ 12/12.py
@@ -1,17 +1,14 @@
 from itertools import islice,count
-triangle_number=lambda n:int(n*0.5*(n+1))
-max_no_divisor=500
-num=1;n=1;max_=0
-divisors=0
+triangle_num=lambda n:int(n*(n+1)*0.5)
+divisors=0;n=1;max_=0
 while(divisors<=500):
-    divisors=0
-    for i in islice(count(1),num):
-        if num%i==0:   
-            divisors+=1
-        if divisors+num-i<max_no_divisor:
-            break
-        if divisors>max_:
-            print(num,divisors,i)
-            max_=divisors
-    num=triangle_number(n+1);n+=1
-print(num,divisors)
+    num=triangle_num(n)
+    factors=set()
+    for i in islice(count(1), int(num**0.5+1)):
+        if(num%i==0):
+            factors.update({i,num//i})
+    divisors=len(factors)
+    if divisors>max_:
+        max_=divisors
+    n+=1
+print(int(n*(n-1)*0.5))
