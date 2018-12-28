1891f34ba0b7342933880844dcc4dab99feeb2d3
SoyujBasnet <bsoyuj@gmail.com>
2018-12-28 14:53:08 +0545

    ....42

---
+++ 42/42.py
@@ -0,0 +1,14 @@
+def value(str_):
+    list_=list(str_)
+    sum_=0
+    for alphabet in list_:
+        sum_+=ord(alphabet)-64
+    return sum_
+triangle_nums={int(n*(n+1)*0.5) for n in range(0,100)}
+with open('p042_words.txt','r') as file:
+    total=0
+    words_list=file.read()[1:-1].split('","')
+    for word in words_list:
+        value_=value(word)
+        if value_ in triangle_nums:total+=1
+    print(total)
