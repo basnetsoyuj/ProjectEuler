581d39da6c8d595f68413a48595ceea5f8f20241
SoyujBasnet <bsoyuj@gmail.com>
2018-12-28 18:14:12 +0545

    42 improvement

--- 42/42.py
+++ 42/42.py
@@ -4,7 +4,7 @@ def value(str_):
     for alphabet in list_:
         sum_+=ord(alphabet)-64
     return sum_
-triangle_nums={int(n*(n+1)*0.5) for n in range(0,100)}
+triangle_nums={int(n*(n+1)*0.5) for n in range(0,80)}
 with open('p042_words.txt','r') as file:
     total=0
     words_list=file.read()[1:-1].split('","')
