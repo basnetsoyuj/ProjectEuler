5f31435b4ec64bdb034e4124681ea3d09a545ac9
Soyuj Jung Basnet <bsoyuj@gmail.com>
2021-11-02 22:59:25 +0400

    Update problem 10 with better method

--- 10/10.py
+++ 10/10.py
@@ -1,13 +1,22 @@
-from itertools import islice,count
-def is_prime(n):
-    for i in islice(count(2),int(n**0.5-1)):
-        if n%i==0:
-            return 0
-    return 1
-def sum_returner(n):
-    sum_=0
-    for i in islice(count(2),n-2):
-        if is_prime(i):
-            sum_+=i
-    return sum_
-print(sum_returner(2000000))
+# inclusive upper bound; less than 2000000 in question
+X = 2000000 - 1
+
+seen = set()
+sum_ = 0
+
+# similar to a prime seive
+# for loop to add all the composite numbers
+for i in range(2, X + 1):
+    if i not in seen:
+        # start with the 2nd multiple of the number
+        n = 2 * i
+        while n < X + 1:
+            if n not in seen:
+                # add all the multiples
+                sum_ += n
+                seen.add(n)
+            n += i
+            
+# (sum of all primes) = (the total sum of all numbers till X) - (sum of all composite numbers)
+# -1 since 1 is not included in the loop
+print(X*(X+1)/2 - sum_ - 1)
